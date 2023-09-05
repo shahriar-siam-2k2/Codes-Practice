56f3f7f9d5e7745a2cb0f0d0f9f495ac7cc6d195
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2023-09-05 16:05:01 +0600

    Initial

---
+++ Patterns/Star pattern printing-1/starPat-1.cpp
@@ -0,0 +1,25 @@
+/*
+*
+* *
+* * *
+* * * *
+* * * * *
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << endl << "Enter pattern range: ";
+    cin >> n;
+    cout << endl;
+
+    for(int i=1 ; i<=n ; i++){
+        cout << "*";
+        for(int j=1 ; j<i ; j++){
+            cout << " *";
+        }
+        cout << endl;
+    }
+    return 0;
+}
