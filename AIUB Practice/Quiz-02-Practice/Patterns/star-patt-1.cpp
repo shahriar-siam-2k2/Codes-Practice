62d1ba21c9eca77fff3fa23f1fb65f37060d0236
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2023-10-18 04:02:08 +0600

    AIUB quiz-2 practice

---
+++ AIUB Practice/Quiz-02-Practice/Patterns/star-patt-1.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+/*
+    *
+    **
+    ***
+    ****
+    *****
+*/
+    // int n;
+    // cin >> n;
+    // for(int i=0 ; i<n ; i++){
+    //     for(int j=0 ; j<i+1 ; j++){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+/*
+    *****
+    ****
+    ***
+    **
+    *
+*/
+    // int n;
+    // cin >> n;
+    // for(int i=0 ; i<n ; i++){
+    //     for(int j=0 ; j<n-i ; j++){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+/*
+        *
+       **
+      ***
+     ****
+    *****
+*/
+    // int n;
+    // cin >> n;
+    // for(int i=0 ; i<n ; i++){
+    //     for(int j=i+1 ; j<n ; j++){
+    //         cout << " ";
+    //     }
+    //     for(int k=0 ; k<(i+1) ; k++){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+/*
+    *****
+     ****
+      ***
+       **
+        *
+*/
+    // int n;
+    // cin >> n;
+    // for(int i=0 ; i<n ; i++){
+    //     for(int j=0 ; j<i ; j++){
+    //         cout << " ";
+    //     }
+    //     for(int k=i ; k<n ; k++){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+    
+/*
+       *
+      ***
+     *****
+    *******
+*/
+    // int n;
+    // cin >> n;
+    // for(int i=0 ; i<n ; i++){
+    //     for(int j=i ; j<n-1 ; j++){
+    //         cout << " ";
+    //     }
+    //     for(int k=0 ; k<(2*i)+1 ; k++){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+/*
+    *******
+     *****
+      ***
+       *
+*/
+    // int n;
+    // cin >> n;
+    // for(int i=0 ; i<n ; i++){
+    //     for(int j=0 ; j<i ; j++){
+    //         cout << " ";
+    //     }
+    //     for(int k=0 ; k<(2*(n-i)-1) ; k++){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+}
