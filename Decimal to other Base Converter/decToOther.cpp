b3281881064ac7968bc16cf70314c34d874defe1
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2023-12-17 02:12:46 +0600

    17-12-23

---
+++ Decimal to other Base Converter/decToOther.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long int dec, div;
+    div = dec;
+
+    cout << endl << "Enter Decimal Number: ";
+    cin >> dec;
+
+    while(div != 0){
+        
+    }
+    return 0;
+}
